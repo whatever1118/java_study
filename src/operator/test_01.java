8a5a48e05c35d2d0aa0116ad612bb1b86520c193
xcw <1518970388@qq.com>
2022-09-14 16:59:59 +0800

    java_test01_final

--- src/operator/test_01.java
+++ src/operator/test_01.java
@@ -1,12 +1,15 @@
 package operator;
-
+import base.*;//导入某个包下的所有类
 public class test_01 {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
+        boolean c = true;
+        boolean d = false;
+        int e = 3;
+        System.out.println("c&&d:"+(c&&d));
         System.out.println(a+b);
-        System.out.println(a);
-        System.out.println(a);
+        System.out.println(e<<2);//位运算，二进制左移两位
         System.out.println(a);
     }
 }
